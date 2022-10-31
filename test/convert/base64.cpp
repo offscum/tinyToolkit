312d7986412451e1c0aed49bfa922f2de6c8067d
huangmengmeng0526@gmail.com <huangmengmeng0526@gmail.com>
2022-10-31 16:50:49 +0800

    refactor(test/convert): 定义

--- test/convert/base64.cpp
+++ test/convert/base64.cpp
@@ -12,96 +12,96 @@
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_Char)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(src.c_str()), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_UChar)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(reinterpret_cast<const uint8_t *>(src.c_str())), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_String)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(src), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_Char_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(src.c_str(), 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_UChar_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(reinterpret_cast<const uint8_t *>(src.c_str()), 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Encode_String_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLw==";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Encode(src, 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_Char)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(src.c_str()), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_UChar)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(reinterpret_cast<const uint8_t *>(src.c_str())), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_String)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(src), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_Char_Length)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(src.c_str(), 29), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_UChar_Length)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(reinterpret_cast<const uint8_t *>(src.c_str()), 29), dst);
 }
 
 TOOLKIT_TEST_CASE(Base64, Base64Decode_String_Length)
 {
-	std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "aHR0cHM6Ly93d3cuYmFpZHUuY29tLz9wMT0xMjMmcDI9dXVpZA==";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::Base64Decode(src, 29), dst);
 }
--- test/convert/url.cpp
+++ test/convert/url.cpp
@@ -12,96 +12,96 @@
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_Char)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(src.c_str()), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_UChar)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(reinterpret_cast<const uint8_t *>(src.c_str())), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_String)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(src), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_Char_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(src.c_str(), 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_UChar_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(reinterpret_cast<const uint8_t *>(src.c_str()), 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlEncode_String_Length)
 {
-	std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
-	std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
+	const std::string src = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string dst = "https%3A%2F%2Fwww.baidu.com%2F";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlEncode(src, 22), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_Char)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(src.c_str()), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_UChar)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(reinterpret_cast<const uint8_t *>(src.c_str())), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_String)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/?p1=123&p2=uuid";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(src), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_Char_Length)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(src.c_str(), 30), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_UChar_Length)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(reinterpret_cast<const uint8_t *>(src.c_str()), 30), dst);
 }
 
 TOOLKIT_TEST_CASE(Url, UrlDecode_String_Length)
 {
-	std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
-	std::string dst = "https://www.baidu.com/";
+	const std::string src = "https%3A%2F%2Fwww.baidu.com%2F%3Fp1%3D123%26p2%3Duuid";
+	const std::string dst = "https://www.baidu.com/";
 
 	TOOLKIT_EXPECT_STR_EQ(tinyToolkit::convert::UrlDecode(src, 30), dst);
 }
